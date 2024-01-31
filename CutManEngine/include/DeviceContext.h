4ddf4650cc3ee75e66a5a090a32b5b24a02abc87
jhonywheeler <jhonywheeler@gmail.com>
2024-01-31 10:26:14 -0600

    Main - Implementation of the DepthStencilView
    DepthStencilView
    DeviceContext

--- CutManEngine/include/DeviceContext.h
+++ CutManEngine/include/DeviceContext.h
@@ -19,6 +19,12 @@ public:
 	void
 		destroy();
 
+	void
+		ClearDephtStencilView(ID3D11DepthStencilView* pDepthStencilView,
+			unsigned int ClearFlags,
+			float Depth,
+			unsigned int Stencil);
+
 public:
 	ID3D11DeviceContext* m_deviceContext = nullptr;
 };
